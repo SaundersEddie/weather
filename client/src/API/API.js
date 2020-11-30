a7f4b65f482dcdd654e00a7b4d9ec441572e0790
Eddie Saunders <saunders.eddie@outlook.com>
2020-11-30 18:38:46 -0500

    Code clean up added API.js

--- client/src/API/API.js
+++ client/src/API/API.js
@@ -1,2 +1,11 @@
 import axios from 'axios';
 
+// Call our server side APIS
+
+export default {
+    getWeatherByZip: function (zipcode) {
+        return axios.get('/weather/zipcode');
+    }
+}
+
+
