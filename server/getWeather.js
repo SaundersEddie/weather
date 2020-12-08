30948f4d343c7f6effd141aa3f0fddbf3f9f2b53
Eddie Saunders <saunders.eddie@outlook.com>
2020-12-08 09:48:45 -0500

    Added in a few axios calls to world clock and weather for testing

---
+++ server/getWeather.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+console.log ('Axios imported');
+
+var options = {
+    method: 'GET',
+    url: 'https://weatherapi-com.p.rapidapi.com/forecast.json',
+    params: {q: 'London', days: '3'},
+    headers: {
+      'x-rapidapi-key': '8a6cd7a64emsh3dd70caa88d8d20p193484jsn53fd404332c5',
+      'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com'
+    }
+  };
+  
+  axios.request(options).then(function (response) {
+      console.log(response.data);
+  }).catch(function (error) {
+      console.error(error);
+  });
--- server/server.js
+++ server/server.js
@@ -1,4 +1,35 @@
 // '8a6cd7a64emsh3dd70caa88d8d20p193484jsn53fd404332c5'
 
-import Axios from 'axios';
+import axios from 'axios';
 console.log ('Axios imported');
+
+var options = {
+    method: 'GET',
+    url: 'https://weatherapi-com.p.rapidapi.com/forecast.json',
+    params: {q: 'London', days: '3'},
+    headers: {
+      'x-rapidapi-key': '8a6cd7a64emsh3dd70caa88d8d20p193484jsn53fd404332c5',
+      'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com'
+    }
+  };
+  
+  axios.request(options).then(function (response) {
+      console.log(response.data);
+  }).catch(function (error) {
+      console.error(error);
+  });
+
+  var options = {
+    method: 'GET',
+    url: 'https://world-clock.p.rapidapi.com/json/est/now',
+    headers: {
+      'x-rapidapi-key': '8a6cd7a64emsh3dd70caa88d8d20p193484jsn53fd404332c5',
+      'x-rapidapi-host': 'world-clock.p.rapidapi.com'
+    }
+  };
+  
+  axios.request(options).then(function (response) {
+      console.log(response.data);
+  }).catch(function (error) {
+      console.error(error);
+  });
