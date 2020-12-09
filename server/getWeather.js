69535d41bd9b25048c91a68e6157ac6fc7dff289
Eddie Saunders <saunders.eddie@outlook.com>
2020-12-08 19:05:57 -0500

    Testing API call requests

--- server/getWeather.js
+++ server/getWeather.js
@@ -4,7 +4,7 @@ console.log ('Axios imported');
 var options = {
     method: 'GET',
     url: 'https://weatherapi-com.p.rapidapi.com/forecast.json',
-    params: {q: 'London', days: '3'},
+    params: {q: 'Los Angeles', days: '3'},
     headers: {
       'x-rapidapi-key': '8a6cd7a64emsh3dd70caa88d8d20p193484jsn53fd404332c5',
       'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com'
