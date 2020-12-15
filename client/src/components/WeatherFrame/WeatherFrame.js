295bbb3dcd1f4b7c4a9bf1b1d228db6be97a76b4
Eddie Saunders <saunders.eddie@outlook.com>
2020-12-15 08:53:16 -0500

    Added in some tests for location searched, updated states in both didmount and onSubmit

--- client/src/components/WeatherFrame/WeatherFrame.js
+++ client/src/components/WeatherFrame/WeatherFrame.js
@@ -2,24 +2,44 @@ import axios from 'axios';
 import React, { Component } from 'react';
 import { Button, Form, FormGroup } from 'reactstrap';
 
+const DisplayWeather = props => {
+    return (
+    <h1> Display out Weather here</h1>
+    // <h1>{props.ourWeather.location.name}</h1>
+    )
+}
 export default class weatherFrame extends Component {
     constructor (props) {
     super (props);
-
 
     this.onChangeOurSearch = this.onChangeOurSearch.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
 
     this.state = {
-        ourSearch: ''
+        ourSearch: '',
+        ourWeather: '',
+    };
     }
-}
 
     componentDidMount() {
         this.setState({
-            ourSearch: ''
+            ourSearch: '',
+            ourWeather: 'settingstate',
         })
-    }
+
+        // Call our weather API here to default populate
+        const defaultWeather = "38.89511 -77.03637"
+        axios.get(`/weather/${defaultWeather}`)
+            .then(res => { 
+                // console.log ('Our Search: ', this.state.ourSearch)
+                this.setState({ourSearch: ''})
+                this.setState ({ourWeather: res.data})
+                console.log ("ourWeather:", this.state.ourWeather)
+            })
+            .catch ((error) => {
+                console.log ("Something went pearshaped")
+            });
+        }
 
     onChangeOurSearch(e) {
         this.setState({
@@ -29,13 +49,23 @@ export default class weatherFrame extends Component {
 
     onSubmit(e) {
         e.preventDefault();
+        // console.log ("Submit clicked");
         axios.get(`/weather/${this.state.ourSearch}`)
-            .then (res => console.log (res.data));
+        .then(res => { 
+            console.log ('Our Search: ', this.state.ourSearch)
+            this.setState({ourSearch: ''})
+            this.setState ({ourWeather: res.data})
+            console.log ("ourWeather onSubmit:", this.state.ourWeather)
+        })
+        .catch ((error) => {
+            console.log ("Something went pearshaped")
+        });
     }
 
     render() {
         return (
         <div>
+            {/* Weather Search */}
             <h1>Weather Search</h1>
             <Form onSubmit={this.onSubmit}>
                 <div className="FormGroup">
@@ -55,6 +85,8 @@ export default class weatherFrame extends Component {
                             />
                     </div>
             </Form>
+            {/* Our Weather display */}
+           {this.DisplayWeather}
         </div>
         )
     }
